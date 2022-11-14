354734b3a18bf978220779673bacfc02b7cfdee4
Oscar Mejia <102716824+AlwayzCesta@users.noreply.github.com>
2022-11-14 16:05:24 -0500

    AlwayzCesta

--- proyecto_back/config/database.js
+++ proyecto_back/config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
-const URI = 'colocar_tu_uri';
+const host = "localhost";
+const port = "27017";
+const db = "hr";
 
 exports.mongoConnect = () => {
-    const mongoStringConnection = URI;
+    const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
     mongoose.connect(mongoStringConnection);
     mongoose.Promise = global.Promise;
     const dbConnection = mongoose.connection;
