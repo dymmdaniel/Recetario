3d289991778ea72fd698da7d9f236cb41fcb7a93
Laura102099 <112049374+Laura102099@users.noreply.github.com>
2022-11-14 19:34:25 -0500

    crud_tabla_menu

--- proyecto_back/controllers/Menu.controller.js
+++ proyecto_back/controllers/Menu.controller.js
@@ -28,3 +28,58 @@ exports.create = function(req,res){
     })
 
 }
+
+exports.find = function(req,res){
+    Menu.find(function(err,menus){
+        res.json(menus)
+    })
+}
+
+exports.findOne = function(req,res){
+    Menu.findOne({nombreReceta: req.params.nombreReceta},function(err,menus){
+        res.json(menus)
+    })
+}
+
+exports.update = function(req,res){
+    let menu = {
+        usuarioId: req.body.usuarioId,
+        clasificacionId: req.body.clasificacionId,
+        nombreReceta: req.body.nombreReceta,
+        descripcion: req.body.descripcion,
+        diaSemana: req.body.diaSemana,
+        diaPreparacion: req.body.diaPreparacion
+    }
+    Menu.findOneAndUpdate({nombreReceta: req.params.nombreReceta},{$set:menu},function(err){
+        if(err){
+            console.error(err),
+            response.exito = false,
+            response.msg = "Error al moficiar el menú " + err
+            res.json(response)
+            return
+        }
+
+        response.exito = true,
+        response.msg = "El menú se modificó correctamente"
+        res.json(response)
+    })
+    
+}
+
+exports.remove = function(req,res){
+    
+    Menu.findOneAndDelete({nombreReceta: req.params.nombreReceta}, function(err){
+        if(err){
+            console.error(err),
+            response.exito = false,
+            response.msg = "Error al eliminar el menú"
+            res.json(response)
+            return
+        }
+
+        response.exito = true,
+        response.msg = "El menú ha sido eliminadó"
+        res.json(response)
+    })
+}
+
--- proyecto_back/routes/Menu.routes.js
+++ proyecto_back/routes/Menu.routes.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const MenuController = require("../controllers/Menu.controller")
 
 router.post("/",MenuController.create)
+router.get("/", MenuController.find)
+router.get("/:nombreReceta", MenuController.findOne)
+router.put("/:nombreReceta", MenuController.update)
+router.delete("/:nombreReceta", MenuController.remove)
 
 
 module.exports = router
