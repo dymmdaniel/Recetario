ecd92a3855841e8fee35e6be05d3416c24efb974
Oscar Mejia <102716824+AlwayzCesta@users.noreply.github.com>
2022-11-25 20:58:30 -0500

    navbar_component

--- proyecto_front/src/App.js
+++ proyecto_front/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
- 
+      
+      <Navbar/>
+      
     </div>
   );
 }
--- proyecto_front/src/components/navbar/navbar.js
+++ proyecto_front/src/components/navbar/navbar.js
@@ -1,1 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar,Card } from "react-bootstrap";
+import './navbar.css'
+export default class navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+    
+      <Navbar id="Nav">
+        <Container>
+          <Navbar.Brand href="#home"><h3 id="titulo">EZzz Gourmet</h3></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features"><h5 id="prim">Descubre</h5></Nav.Link>
+              <Nav.Link href="#pricing"><h6 id="segu">Categoria</h6></Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets"><h6 id="prim">Iniciar Sesión</h6></Nav.Link>
+              <Card id="arc">
+              <Nav.Link  href="#memes"><h6 id="ter">Registrarse</h6> </Nav.Link>
+              </Card>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+    );
+  }
+}
